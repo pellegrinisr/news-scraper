70b244e118874771a9e191c648604b55a88b6c71
Stephen Pellegrini <spellegrini156@gmail.com>
2018-10-03 00:05:39 -0700

    trying to troubleshoot heroku

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static('public'));
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 //configure mongoose
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 //use the router
